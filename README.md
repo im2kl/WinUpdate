d200f33b416bd426b33cc1cbd71a58a0e84b8609
Luis Santos <luisfcs@hotmail.co.uk>
2020-09-02 20:13:20 +0100

    Update Search
    looked into searching for updates and session creation with MSServers

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-WinUpdate
+## WinUpdate
+
+
+Search, dowload and install windows updates. 
+
+https://docs.microsoft.com/en-us/windows/win32/api/_wua/
--- WindowsUpdateAgent/Program.cs
+++ WindowsUpdateAgent/Program.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WUApiLib;
 
 namespace WindowsUpdateAgent
 {
-    class Program
+    class WinUpdate
     {
-        static void Main(string[] args)
+        static void Main()
         {
+
+            UpdateSession session = new UpdateSession();
+
+            var lookup = session.CreateUpdateSearcher();
+            
+            Console.WriteLine("Searching... async");
+            var results = lookup.Search(""); // parameters for search
+
+            foreach (IUpdate upd in results.Updates)
+            {
+                // these can be leveraged for a profile install 
+                if (upd.AutoSelectOnWebSites &&
+                    !upd.InstallationBehavior.CanRequestUserInput &&
+                    !upd.IsInstalled &&
+                    !upd.IsHidden &&
+                    (upd.InstallationBehavior.RebootBehavior == InstallationRebootBehavior.irbNeverReboots))
+                {
+                    Console.WriteLine(upd.Title);
+                }
+            }
+            Console.ReadKey();
         }
     }
 }
