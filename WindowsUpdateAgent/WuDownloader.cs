2d061ff12d1353fbbfc72a27cbf3081ba280ad7e
Luis Santos <luisfcs@hotmail.co.uk>
2020-09-27 21:51:22 +0100

    added full pseudo for downlaod and inatallation

--- WindowsUpdateAgent/WuDownloader.cs
+++ WindowsUpdateAgent/WuDownloader.cs
@@ -11,21 +11,49 @@ namespace WindowsUpdateAgent
     class WuDownloader
     {
         UpdateCollection updatecollection = new UpdateCollection();
-
-        private IUpdate Entry = null;
+        
+        // private IUpdate Entry = null;
         public void Download(List<WuModel> updateList)
         {
             foreach(WuModel updatex in updateList)
             {
-
-
-
-
-
+                
+                // convert update back to Iupdate :/
             }
 
         }
+        public void GetWindowsUpdates()
+        {
 
+            UpdateSession session = new UpdateSession();
+            // Download updates
+            UpdateDownloader downloader = session.CreateUpdateDownloader();
+            downloader.Updates = updatecollection;
+            downloader.Download();
+
+            // Add updates to install list
+            UpdateCollection updatesToInstall = new UpdateCollection();
+            foreach (IUpdate update in updatecollection)
+                if (update.IsDownloaded)
+                    updatesToInstall.Add(update);
+
+            // Create installer from list
+            IUpdateInstaller installer = session.CreateUpdateInstaller();
+            installer.Updates = updatesToInstall;
+
+            // Install updates
+            IInstallationResult installationRes = installer.Install();
+
+            // List update result after installation
+            for (int i = 0; i < updatesToInstall.Count; i++)
+            {
+                if (installationRes.GetUpdateResult(i).HResult == 0)
+                    Console.WriteLine("Installed : " + updatesToInstall[i].Identity.UpdateID[0]);
+                else
+                    Console.WriteLine("Failed : " + updatesToInstall[i].Identity.UpdateID[0]);
+            }
+
+        }
 
 
 
