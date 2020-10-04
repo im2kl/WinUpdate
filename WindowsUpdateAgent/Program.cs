c5bf29d4fa6962fcfaf450e9d24dff70727b4f2d
Luis Santos <luisfcs@hotmail.co.uk>
2020-10-04 20:01:56 +0100

    settings check for update page.

--- WindowsUpdateAgent/Program.cs
+++ WindowsUpdateAgent/Program.cs
@@ -15,6 +15,34 @@ namespace WindowsUpdateAgent
     {
         static void Main()
         {
+            Console.WriteLine("start");
+            WuProfileSettings settings = new WuProfileSettings();
+
+            if (settings.IsUpdatePageHidden())
+            {
+                Console.WriteLine("itshidden");
+                settings.HideUpdatePage(true);
+            }
+
+            var x = settings.GetAU();
+            Console.WriteLine(x);
+
+            settings.ConfigAU(WuProfileSettings.AUOptions.Disabled);
+
+            var y = settings.GetAU();
+            Console.WriteLine(y);
+
+            Thread.Sleep(4000);
+            Console.WriteLine("set default");
+            
+            settings.ConfigAU(WuProfileSettings.AUOptions.Default);
+
+            Console.WriteLine("sleep");
+            Thread.Sleep(10000);
+            Console.WriteLine("hello");
+
+
+
             WuDetect searcher = new WuDetect();
             var updatelistjson = searcher.GetUpdateList();
             //var updateList = JsonConvert.DeserializeObject<List<WuModel>>(updatelistjson);
@@ -22,6 +50,8 @@ namespace WindowsUpdateAgent
 
             Console.WriteLine(updatelistjson);
 
+
+            
 
             //Identity updateidentity = new Identity();
             //List<Identity> superceededidentity = new List<Identity>();
